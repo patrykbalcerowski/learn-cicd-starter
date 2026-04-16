eadeaba1c435d1f31aba38b2ee74689922efccf9
Patryk Balcerowski <patrykbalc@gmail.com>
2026-04-16 20:59:16 +0200

    Add security check and pass

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -89,10 +91,13 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: time.Second,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	cleanPort := strings.ReplaceAll(port, "\n", "")
+	cleanPort = strings.ReplaceAll(cleanPort, "\r", "")
+	log.Printf("Serving on port: %s\n", cleanPort)
 	log.Fatal(srv.ListenAndServe())
 }
